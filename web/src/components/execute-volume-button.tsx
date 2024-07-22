2065ce9fb3f508379e9fcba4280be1fa2c60c2dd
Jatin Suri <surijatin@yahoo.com>
2024-07-22 13:30:53 -0400

    ran linter

--- web/src/components/execute-volume-button.tsx
+++ web/src/components/execute-volume-button.tsx
@@ -7,15 +7,15 @@ export const ExecuteVolumeButton: React.FC<ButtonProps> = ({ onClick, isDisabled
   const { t } = useTranslation('plugin__logging-view-plugin');
 
   return (
-      <Tooltip content={<div>Gives the volume of data for a given label</div>}>
-        <Button
-          variant="secondary"
-          data-test={TestIds.ExecuteVolumeButton}
-          onClick={onClick}
-          isDisabled={isDisabled}
-        >
-          {t('Explain Log Volume')}
-        </Button>
-      </Tooltip>
+    <Tooltip content={<div>Gives the volume of data for a given label</div>}>
+      <Button
+        variant="secondary"
+        data-test={TestIds.ExecuteVolumeButton}
+        onClick={onClick}
+        isDisabled={isDisabled}
+      >
+        {t('Explain Log Volume')}
+      </Button>
+    </Tooltip>
   );
 };
